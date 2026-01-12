9b9ee27f061cfebdb5f655c9f10a105e74d6e48f
Petr Pivoňka <petrpiv1@gmail.com>
2026-01-13 00:25:19 +0100

    Init

---
+++ Tests/SimplestGitSourceGenerator.Tests/GitInfoGeneratorTests.cs
@@ -0,0 +1,13 @@
+﻿namespace SimplestGitSourceGenerator.Tests;
+
+public class GitInfoGeneratorTests
+{
+    [Fact]
+    public void SimplestGit_IsNotEmpty()
+    {
+        Assert.NotEmpty(SimplestGit.CommitHash);
+        Assert.NotEmpty(SimplestGit.CommitDate);
+        Assert.NotEmpty(SimplestGit.Branch);
+        Assert.NotEmpty(SimplestGit.Tag);
+    }
+}
